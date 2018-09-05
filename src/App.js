71c100e7d771772b40b547924c5c2424933cc5b3
Roger Graves <rogergraves@gmail.com>
2018-09-05 16:30:23 -0700

    Create Login component and customize logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './icon-round-question_mark.svg';
 import './App.css';
+import Login from './components/Login'
 
 class App extends Component {
   render() {
@@ -8,10 +9,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Would You Rather</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+            <Login />
         </p>
       </div>
     );
