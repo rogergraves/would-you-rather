447e9210a1d1254e0189528473226e590f2bfbd2
Roger Graves <rogergraves@gmail.com>
2018-09-06 15:41:39 -0700

    Move list type selection over to home page and add filtering functionality

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -4,39 +4,13 @@ import '../QuestionList.css'
 
 class QuestionList extends Component {
 
-    state = {
-        view_answered: false
-    }
-
-    toggleViewAnswer = (e) => {
-        this.setState(prevState => ({
-            view_answered: !prevState.view_answered
-        }))
-    }
 
     render() {
-        console.log("viewed_answered", this.state.view_answered)
+        console.log("Render called")
 
         return (
             <div>
-                <h3>Question List</h3>
-                <div class='ListTypeSelect'>
-                    <input type='radio'
-                           value='true'
-                           name='view_answered'
-                           checked={this.state.view_answered}
-                           onChange={this.toggleViewAnswer}
-                    /> Answered
-                </div>
-                <div className='ListTypeSelect'>
-                    <input type='radio'
-                           value='false'
-                           name='view_answered'
-                           checked={!this.state.view_answered}
-                           onChange={this.toggleViewAnswer}
-                    /> Unanswered
-                </div>
-                
+
             </div>
         )
     }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,72 @@
 import React, { Component } from 'react'
-import QuestionList from "../components/QuestionList";
+import * as DataAPI from "../utils/_DATA";
 
 class Home extends Component {
+
+    state = {
+        view_answered: false,
+        loading: true
+    }
+
+    toggleViewAnswer = (e) => {
+        this.setState(prevState => ({
+            view_answered: !prevState.view_answered
+        }))
+        this.getQuestions()
+    }
+
+    getQuestions = () => {
+        DataAPI._getQuestions().then((obj) => {
+            const list = Object.values(obj)
+
+            console.log("filterQuestions", this.filterQuestions(list))
+            this.setState({
+                loading: false
+            })
+        })
+    }
+
+    componentDidMount() {
+        this.getQuestions()
+    }
+
+    filterQuestions = (master_list) => {
+        return master_list.filter((question) => this.hasVotes(question) === this.state.view_answered)
+    }
+
+    hasVotes(question) {
+        return (
+            question.optionOne.votes.length > 0 || question.optionTwo.votes.length > 0
+        )
+    }
+
 
     render() {
         return (
             <div>
                 <h2>Home Page</h2>
-                <QuestionList />
+                <div className='ListTypeSelect'>
+                    <input type='radio'
+                           value='true'
+                           name='view_answered'
+                           checked={this.state.view_answered}
+                           onChange={this.toggleViewAnswer}
+                    /> Answered
+                </div>
+                <div className='ListTypeSelect'>
+                    <input type='radio'
+                           value='false'
+                           name='view_answered'
+                           checked={!this.state.view_answered}
+                           onChange={this.toggleViewAnswer}
+                    /> Unanswered
+                </div>
+                {this.state.loading ? (
+                    <h4>Loading...</h4>
+                ) : (
+                    <h4>List</h4>
+                )}
+
             </div>
         )
     }
