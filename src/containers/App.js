69c8cf7ba4d1716c3036561c47cc78d3a666814c
Roger Graves <rogergraves@gmail.com>
2018-09-06 19:23:29 -0700

    Add react, react-redux and get base install of it working with users and questions

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react'
 import logo from '../icon-round-question_mark.svg'
 import '../App.css'
 import Home from './Home'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return (
       <div className="App">
@@ -17,4 +22,4 @@ class App extends Component {
   }
 }
 
-export default App
+export default connect()(App)
